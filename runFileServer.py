7d5ad09b903b4a41a375fde376579a3c41fbbc70
George Ebeling <ebelingg@mail.gvsu.edu>
2021-03-03 10:06:40 -0500

    Added option to choose the welcome port.

--- runFileServer.py
+++ runFileServer.py
@@ -2,12 +2,27 @@ import FileServerManager
 import signal
 import sys
 
+
 def signal_handler(sig, frame):
     print("Closing the server")
     myServer.closeControlServer()
 
 
-if __name__ == '__main__':
-    signal.signal(signal.SIGINT, signal_handler) # I thought this was necessary for clean closing of the server
-    myServer = FileServerManager.FileServer("localhost", 1609)
+def getPortNumber():
+    # If the user gave a port number, set it to that. Otherwise, use the default
+    port = -1
+    try:
+        port = int(sys.argv[1])
+    except IndexError:
+        port = int(1609)
+    return port
 
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+
+    welcomePort = int(getPortNumber())
+    if 0 < welcomePort <= 65535:
+        myServer = FileServerManager.FileServer("localhost", welcomePort)
+    else:
+        print("User gave an invalid port number")

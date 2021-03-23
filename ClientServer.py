7b092919ac958bb0b12ca0d43b336709f58b4058
George <ebelingg@mail.gvsu.edu>
2021-03-23 16:09:35 -0400

    Added the client server skeleton

---
+++ ClientServer.py
@@ -0,0 +1,24 @@
+
+
+if __name__ == 'main':
+    pass
+
+
+def Start(self):
+    print("not starting server")
+    pass
+
+
+def Listen(self):
+    print("not listening")
+    pass
+
+
+def Send(self):
+    print("not sending")
+    pass
+
+
+def Close(self):
+    print("not closing")
+    pass
